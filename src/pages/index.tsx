b3f0de7035c1688b43b080d80a837e5a250b3d3a
Eduardo <eduardo@iMac-de-Eduardo.local>
2022-09-09 00:19:52 -0300

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,17 @@ import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1>iFinance</h1>
+    <main className={styles.containerFluid}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img src="/assets/images/logo-vertical.svg" alt="Logo iFinance" />
+          <h1>A melhor forma de <br />
+          <span>Organizar</span> suas financas</h1>
+          <a href="">Acessar</a>
+        </div>
+        <img src="/assets/images/home-image.svg" alt="Mulher Organizando sua renda" />
+      </div>
+    </main>
   )
 }
 
