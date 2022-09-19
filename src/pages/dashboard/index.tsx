c4059bac92680ce08f0dda847500c2c7a22a316c
Eduardo <eduardo@iMac-de-Eduardo.local>
2022-09-19 15:30:43 -0300

    monthly expenses table

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { getSession, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Navbar from "../../components/Navbar";
 
-import styles from '../../styles/Dashboard.module.scss';
+import Styles from '../../styles/Dashboard.module.scss';
 import Header from "../../components/Header/Header";
 import Summary from "../../components/Summary/Summary";
 
@@ -11,18 +11,67 @@ export default function Dashboard() {
     const date = new Date();
 
     return (
-        <div className={styles.container}>
+        <div className={Styles.container}>
             <Head>
                 <title>Dashboard | iFinances</title>
             </Head>
             <Navbar />
-            <div className={styles.content}>
+            <div className={Styles.content}>
                 <Header
                     title={'Overview'}
                     subtitle={`${new Intl.DateTimeFormat('pt-BR', { month: "long" }).format(new Date())} - ${date.getFullYear()}`}
                     description="Tenha uma visão geral de todos os seus gastos no mês de Janeiro."
                 />
                 <Summary />
+                <div className={Styles.spending}>
+                    <h2>Gastos de <span>{new Intl.DateTimeFormat('pt-BR', { month: "long" }).format(new Date())}</span></h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Preço</th>
+                                <th>Categoria</th>
+                                <th>Data</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Salário</td>
+                                <td className={true ? Styles.entry : Styles.exit}>+ R$3.099,00</td>
+                                <td>Eletrônicos</td>
+                                <td>01/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td>Monitor Ultrawide 32"</td>
+                                <td className={false ? Styles.entry : Styles.exit}>- R$1.099,00</td>
+                                <td>Eletrônicos</td>
+                                <td>02/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td>Alimentação Casa"</td>
+                                <td className={false ? Styles.entry : Styles.exit}>- R$500,00</td>
+                                <td>Alimentação</td>
+                                <td>03/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td>Suporte Duplo para Monitor</td>
+                                <td className={false ? Styles.entry : Styles.exit}>- R$299,00</td>
+                                <td>Eletrônicos</td>
+                                <td>03/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td>Bolsa de P&D</td>
+                                <td className={true ? Styles.entry : Styles.exit}>+ R$1500,00</td>
+                                <td>Eletrônicos</td>
+                                <td>05/10/2022</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
