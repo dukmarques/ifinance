a207d5ff8588c10c512912b9546b1ab17d85c7fa
dukmarques <eduardomarques@outlook.com>
2022-09-20 23:36:03 -0300

    added transaction registration form

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -12,6 +12,7 @@ import Header from "../../components/Header/Header";
 import Summary from "../../components/Summary/Summary";
 
 import plusImage from "../../../public/assets/icons/plus-small.svg";
+import CloseButton from '../../../public/assets/icons/close-button.svg';
 
 export default function Dashboard() {
     const date = new Date();
@@ -31,9 +32,34 @@ export default function Dashboard() {
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
+                className={Styles.modal}
+                overlayClassName={Styles.Overlay}
             >
-                <h2>Adicionar nova transação!</h2>
-                <button onClick={closeModal}><span>X</span></button>
+                <form>
+                    <h3>Cadastrar Transação</h3>
+                    <input type="text" placeholder="Nome" />
+                    <input type="number" placeholder="Preço" />
+                    <select>
+                        <option value="">Entrada</option>
+                        <option value="">Saída</option>
+                    </select>
+                    <input type="date" />
+                    <select>
+                        <option value="">Nubank</option>
+                        <option value="">Ourocard</option>
+                        <option value="">Caixa</option>
+                    </select>
+                    <input type="text" placeholder="Categoria" />
+                    <select>
+                        <option value="">Minha Transação</option>
+                        <option value="">Transação de Fulano 1</option>
+                        <option value="">Transação de Fulano 2</option>
+                    </select>
+                    <button type="submit">Cadastrar</button>
+                </form>
+                <button className={Styles.closeModal} onClick={closeModal}>
+                    <Image src={CloseButton} width='40' height='40' />
+                </button>
             </Modal>
             <Head>
                 <title>Dashboard | iFinances</title>
@@ -53,6 +79,8 @@ export default function Dashboard() {
                             <tr>
                                 <th>Título</th>
                                 <th>Preço</th>
+                                <th>Pertence</th>
+                                <th>Cartão</th>
                                 <th>Categoria</th>
                                 <th>Data</th>
                             </tr>
@@ -61,6 +89,8 @@ export default function Dashboard() {
                             <tr>
                                 <td>Salário</td>
                                 <td className={true ? Styles.entry : Styles.exit}>+ R$3.099,00</td>
+                                <td>Minha</td>
+                                <td>Nubank</td>
                                 <td>Eletrônicos</td>
                                 <td>01/10/2022</td>
                             </tr>
@@ -68,6 +98,8 @@ export default function Dashboard() {
                             <tr>
                                 <td>Monitor Ultrawide 32&quot;</td>
                                 <td className={false ? Styles.entry : Styles.exit}>- R$1.099,00</td>
+                                <td>Minha</td>
+                                <td>Ourocard</td>
                                 <td>Eletrônicos</td>
                                 <td>02/10/2022</td>
                             </tr>
@@ -75,6 +107,8 @@ export default function Dashboard() {
                             <tr>
                                 <td>Alimentação Casa</td>
                                 <td className={false ? Styles.entry : Styles.exit}>- R$500,00</td>
+                                <td>Minha</td>
+                                <td>Nubank</td>
                                 <td>Alimentação</td>
                                 <td>03/10/2022</td>
                             </tr>
@@ -82,6 +116,8 @@ export default function Dashboard() {
                             <tr>
                                 <td>Suporte Duplo para Monitor</td>
                                 <td className={false ? Styles.entry : Styles.exit}>- R$299,00</td>
+                                <td>Minha</td>
+                                <td>Ourocard</td>
                                 <td>Eletrônicos</td>
                                 <td>03/10/2022</td>
                             </tr>
@@ -89,7 +125,18 @@ export default function Dashboard() {
                             <tr>
                                 <td>Bolsa de P&#38;D</td>
                                 <td className={true ? Styles.entry : Styles.exit}>+ R$1500,00</td>
+                                <td>Minha</td>
+                                <td>Caixa</td>
                                 <td>Eletrônicos</td>
+                                <td>05/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td>iFood</td>
+                                <td className={false ? Styles.entry : Styles.exit}>- R$20,00</td>
+                                <td>Fulano 1</td>
+                                <td>Dinheiro</td>
+                                <td>Alimentação</td>
                                 <td>05/10/2022</td>
                             </tr>
                         </tbody>
