cfbb43b276e3c5cfbdf32b1d06f3712b2ec9a7c7
dukmarques <eduardomarques@outlook.com>
2022-10-11 23:49:33 -0300

    refactor: standardization of navbar images

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react';
 import styles from './Styles.module.scss';
 
 import Image from 'next/image';
+import arrowMenu from '../../../public/assets/icons/arrow-menu.svg';
+import horizontalLogo from '../../../public/assets/images/logo-horizontal.svg';
+import dollarSign from '../../../public/assets/images/dollar-sign.svg';
 import overview from '../../../public/assets/icons/overview.svg';
 import overviewWhite from '../../../public/assets/icons/overview-white.svg';
 import cards from '../../../public/assets/icons/cards.svg';
@@ -13,11 +16,12 @@ import settingsWhite from '../../../public/assets/icons/settings-white.svg';
 import transactions from '../../../public/assets/icons/transactions.svg';
 import transactionsWhite from '../../../public/assets/icons/transactions-white.svg';
 import logout from '../../../public/assets/icons/logout.svg';
-import navbarImage from  '../../../public/assets/images/navbar-image.svg';
+import navbarImage from '../../../public/assets/images/navbar-image.svg';
+import Link from 'next/link';
 
 export default function Navbar() {
-    const {data: session} = useSession();
-    const router  = useRouter();
+    const { data: session } = useSession();
+    const router = useRouter();
 
     const [collapseMenu, setCollapseMenu] = useState(false);
 
@@ -27,17 +31,17 @@ export default function Navbar() {
     }
 
     return (
-        <div className={`${styles.container} ${collapseMenu ? styles.collapse: ''}`}>
+        <div className={`${styles.container} ${collapseMenu ? styles.collapse : ''}`}>
             <a onClick={(e) => handleCollapseMenu(e)} className={styles.expandCollapse}>
-                <img src="/assets/icons/arrow-menu.svg" alt="Expand or collapse menu" />
+                <Image src={arrowMenu} alt="Expand or collapse menu" />
             </a>
 
             <div className={styles.content}>
                 <div className={styles.perfil}>
                     {
-                        !collapseMenu 
-                        ? <img className={styles.logo} src="/assets/images/logo-horizontal.svg" alt="Logo da iFinances" />
-                        : <img className={styles.logo} src="/assets/images/dollar-sign.svg" alt="Logo da iFinances" />
+                        !collapseMenu
+                            ? <Image className={styles.logo} src={horizontalLogo} alt="Logo da iFinances" />
+                            : <Image className={styles.logo} src={dollarSign} alt="Logo da iFinances" />
                     }
                     <img className={styles.photo} src={session?.user?.image!} alt="Foto de Perfil" />
                     <h2>Hello, {session?.user?.name?.substring(0, session.user.name.indexOf(" "))} 👋🏽</h2>
@@ -49,46 +53,46 @@ export default function Navbar() {
 
                 <div className={styles.navigation}>
                     <a onClick={() => router.push('/dashboard')} className={router.pathname === '/dashboard' ? styles.selected : styles.menu}>
-                        { router.pathname === '/dashboard' 
-                            ? <Image src={overviewWhite} width='30' height='30' />
-                            : <Image src={overview} width='30' height='30' />
+                        {router.pathname === '/dashboard'
+                            ? <Image src={overviewWhite} width='30' height='30' alt='Overview icone' />
+                            : <Image src={overview} width='30' height='30' alt='Overview icone' />
                         }
                         <span>Overview</span>
                     </a>
 
                     <a onClick={() => router.push('/dashboard/transactions')} className={router.pathname === '/dashboard/transactions' ? styles.selected : styles.menu}>
-                        { router.pathname === '/dashboard/transactions' 
-                            ? <Image src={transactionsWhite} width='30' height='30' />
-                            : <Image src={transactions} width='30' height='30' />
+                        {router.pathname === '/dashboard/transactions'
+                            ? <Image src={transactionsWhite} width='30' height='30' alt='Icone do menu de transações' />
+                            : <Image src={transactions} width='30' height='30' alt='Icone do menu de transações' />
                         }
                         <span>Transações</span>
                     </a>
 
                     <div onClick={() => router.push('/dashboard/cards')} className={router.pathname === '/dashboard/cards' ? styles.selected : styles.menu}>
-                        { router.pathname === '/dashboard/cards' 
-                            ? <Image src={cardsWhite} width='30' height='30' />
-                            : <Image src={cards} width='30' height='30' />
+                        {router.pathname === '/dashboard/cards'
+                            ? <Image src={cardsWhite} width='30' height='30' alt='Icone do menu de cartões' />
+                            : <Image src={cards} width='30' height='30' alt='Icone do menu de cartões' />
                         }
                         <span>Cartões</span>
                     </div>
 
                     <div onClick={() => router.push('/dashboard/settings')} className={router.pathname === '/dashboard/settings' ? styles.selected : styles.menu}>
-                        { router.pathname === '/dashboard/settings' 
-                            ? <Image src={settingsWhite} width='30' height='30' />
-                            : <Image src={settings} width='30' height='30' />
+                        {router.pathname === '/dashboard/settings'
+                            ? <Image src={settingsWhite} width='30' height='30' alt='Icone do menu de configurações' />
+                            : <Image src={settings} width='30' height='30' alt='Icone do menu de configurações' />
                         }
                         <span>Configurações</span>
                     </div>
 
                     <div onClick={() => signOut()} className={styles.menu}>
-                        <Image src={logout} width='25' height='25' />
+                        <Image src={logout} width='25' height='25' alt='Icone do botão de deslogar' />
                         <span>Deslogar</span>
                     </div>
                 </div>
-                
+
                 <div className={styles.footer}>
-                    <Image src={navbarImage} />
-                    <a href='https://github.com/dukmarques' target="_blank" rel="noreferrer noopener">Develop with 🫶🏽 by Eduardo</a>
+                    <Image src={navbarImage} alt="Imagem do rodapé" />
+                    <Link href='https://github.com/dukmarques' target="_blank" rel="noreferrer noopener">Develop with 🫶🏽 by Eduardo</Link>
                 </div>
             </div>
         </div>
