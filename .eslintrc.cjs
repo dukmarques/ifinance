3bbb4085b67a468151f59d482914e50b35873b8f
dukmarques <eduardomarques@outlook.com>
2024-03-05 23:19:36 -0300

    feat: add tailwindcss and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,46 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    root: true,
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: [
+            'error',
+            4,
+        ],
+        'no-console': [
+            'warn',
+            {
+                allow: ['error'],
+            },
+        ],
+        'array-bracket-newline': [
+            'error',
+            {
+                minItems: 2,
+                multiline: true,
+            },
+        ],
+        'vue/html-indent': [
+            'error',
+            4,
+            {
+                attribute: 1,
+                baseIndent: 1,
+                closeBracket: 0,
+                alignAttributesVertically: true,
+                ignores: [],
+            },
+        ],
+    },
+};
