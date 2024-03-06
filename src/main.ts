e6f68d6a23b922669d7f7a6880d2e353b6348855
dukmarques <eduardomarques@outlook.com>
2024-03-05 21:38:50 -0300

    feat: add files (again)

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+app.mount('#app')
