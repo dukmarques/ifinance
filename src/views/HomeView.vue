7b3f0d764dfedcb0ced81c83598b397eda252e94
dukmarques <eduardomarques@outlook.com>
2024-08-23 00:11:54 -0300

    feat: Update HomeView with logo image and login button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-    <VContainer>
-        <TheWelcome />
+    <VContainer
+        fluid height="100vh"
+        class="bg-primary d-flex justify-center align-center"
+    >
+        <VRow class="d-flex flex-column justify-center align-center ga-10">
+            <VImg
+                :src="logoImg"
+                width="915"
+                cover
+                transition="scroll-y-transition"
+
+            />
+
+            <VBtn
+                size="large"
+                rounded="lg"
+                width="285"
+                height="60"
+                @click="router.push('/login')"
+                color="secondary"
+            >
+                Acessar
+                <template v-slot:append>
+                    <VIcon icon="mdi-arrow-right" color="primary"/>
+                </template>
+            </VBtn>
+        </VRow>
     </VContainer>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import logoImg from '@/assets/logo.png';
+
+const router = useRouter();
+</script>
