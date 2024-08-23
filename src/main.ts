6ef1cdd3f79ba321572e2b89ee7d41fb6bb0776f
dukmarques <eduardomarques@outlook.com>
2024-08-23 00:10:37 -0300

    chore: Update background color of VApp component

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router/router';
 
 const vuetify = createVuetify({
     components,
     directives,
     theme: {
-        defaultTheme: 'dark'
+        defaultTheme: 'dark',
+        themes: {
+            dark: {
+                colors: {
+                    primary: '#359766',
+                    secondary: '#202223',
+                }
+            }
+        }
     },
     icons: {
         defaultSet: 'mdi',
@@ -26,15 +34,15 @@ const vuetify = createVuetify({
             mdi,
         },
     },
-})
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-app.use(vuetify)
-app.use(pinia)
-app.use(router)
+app.use(vuetify);
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
