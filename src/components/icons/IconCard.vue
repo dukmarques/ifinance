dd314cbc32e1b938e14eb3e91273856069af588c
dukmarques <eduardomarques@outlook.com>
2024-09-12 23:55:50 -0300

    feat: add icons

---
+++ src/components/icons/IconCard.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="28" height="30" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20.8224 7.79541H7.10959C6.2681 7.79541 5.58594 8.55888 5.58594 9.50067V19.7322C5.58594 20.674 6.2681 21.4375 7.10959 21.4375H20.8224C21.6639 21.4375 22.346 20.674 22.346 19.7322V9.50067C22.346 8.55888 21.6639 7.79541 20.8224 7.79541Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M5.58594 12.6676H22.346" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+</template>
