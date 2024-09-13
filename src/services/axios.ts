7195938f9cca2109d709eb35e62abdefe209cb8b
dukmarques <eduardomarques@outlook.com>
2024-09-12 23:55:19 -0300

    feat: Add request and response axios and interceptors

---
+++ src/services/axios.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import { useUserStore } from '../stores/user';
+import router from '../router/router';
+
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL as string;
+
+axios.interceptors.request.use((config) => {
+    const userStore = useUserStore();
+
+    config.headers.Authorization = `Bearer ${userStore.accessToken}`;
+    return config;
+});
+
+axios.interceptors.response.use((response) => response, (error) => {
+    if (error.response.status === 401 && error.response.data.message === 'unauthenticated user') {
+        const userStore = useUserStore();
+        userStore.logout();
+        router.push('/login');
+    }
+    return Promise.reject(error);
+});
+
+export { axios, VueAxios };
