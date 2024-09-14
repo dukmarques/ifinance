7cf9a2a587bb6d83f873a407f65b61bc74ce0917
dukmarques <eduardomarques@outlook.com>
2024-09-13 22:48:24 -0300

    refactor: Update NavigationDrawer component

--- src/components/Navigation/NavigationDrawer.vue
+++ src/components/Navigation/NavigationDrawer.vue
@@ -11,36 +11,47 @@
         class="ma-2 mt-5"
         style="height: 95vh"
     >
-        <v-list height="200">
-            <VListItem
-                prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-                subtitle="sandra_a88@gmailcom"
-                title="Sandra Adams"
-            ></VListItem>
-
+        <v-list
+            nav
+            :height="navigation.getRail ? '100' : '240'"
+        >
             <v-list-item>
-                <div class="d-flex justify-start align-center">
-                    <v-btn
-                        :icon="navigation.getRail
-                            ? 'keyboard_double_arrow_right'
-                            : 'keyboard_double_arrow_left'"
-                        variant="text"
-                        @click.stop="navigation.toggleRail"
-                        title="Alternar menu"
+                <div class="d-flex flex-column justify-center align-center">
+                    <v-img
+                        v-if="!navigation.getRail"
+                        :src="logo"
+                        width="100"
+                        transition="scroll-y-transition"
+                        aspect-ratio="16/9"
+                    />
+                    <div
+                        class="box-avatar d-flex justify-center align-center mt-5 mb-3"
+                        :class="{ 'rail': navigation.getRail }"
                     >
-                    </v-btn>
-                    <p v-if="!navigation.getRail" class="text-subtitle-1">Recolher menu</p>
+                        <Icon
+                            icon="fluent:person-48-filled"
+                            :width="!navigation.getRail ? '64' : '48'"
+                        />
+                    </div>
+
+                    <v-list-item-title v-if="!navigation.getRail">
+                        <div class="text-h5">
+                            Hello, {{ getUserFirstName }}
+                        </div>
+                    </v-list-item-title>
+                    <v-list-item-subtitle v-if="!navigation.getRail">
+                        Bem-vindo(a) ao Prosperify.
+                    </v-list-item-subtitle>
                 </div>
             </v-list-item>
         </v-list>
-
-        <VDivider />
 
         <v-list
             nav
             density="compact"
             class="pt-3"
         >
+            <VDivider />
             <v-list-item
                 v-for="(item) in menuItems"
                 :key="`menu-${item.name}`"
@@ -54,10 +65,7 @@
                     class="d-flex align-center"
                     :class="navigation.rail ? 'justify-center': 'justify-start ga-5 pl-5'"
                 >
-                    <v-icon
-                        size="x-large"
-                        :icon="item.icon"
-                    ></v-icon>
+                    <Icon :icon="item.icon" width="34" />
                     <v-list-item-title
                         v-if="!navigation.getRail"
                         class="text-white"
@@ -73,23 +81,44 @@
                 nav
                 density="compact"
             >
-                <VDivider/>
-
                 <v-list-item
                     @click="logout"
                     class="mt-2"
                 >
                     <div
-                        class="d-flex justify-center align-center ga-5"
+                        class="d-flex justify-center align-center ga-2"
                         :class="{ 'justify-center': navigation.getRail }"
                     >
-                        <v-icon
-                            size="large"
-                            icon="logout"
-                        ></v-icon>
+                        <Icon
+                            icon="fluent:arrow-exit-20-regular"
+                            width="24"
+                        />
                         <v-list-item-title v-if="!navigation.getRail" class="text-white">
-                            Sair
+                            <p class="text-subtitle-2">Sair</p>
                         </v-list-item-title>
+                    </div>
+                </v-list-item>
+
+                <VDivider/>
+
+                <v-list-item>
+                    <div class="d-flex justify-center align-center">
+                        <v-btn
+                            variant="text"
+                            @click.stop="navigation.toggleRail"
+                            title="Alternar menu"
+                        >
+                            <Icon
+                                icon="fluent:arrow-swap-28-regular"
+                                width="24"
+                            />
+                            <p
+                                v-if="!navigation.getRail"
+                                class="text-subtitle-2 ml-2"
+                            >
+                                Recolher menu
+                            </p>
+                        </v-btn>
                     </div>
                 </v-list-item>
             </v-list>
@@ -100,13 +129,14 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { Icon } from '@iconify/vue';
 import { useUserStore } from '@/stores/user';
 import { useToast } from '@/stores/toast';
 import { useNavigationStore } from '@/stores/navigation';
+import logo from '@/assets/logo-primary.png';
 
 const drawer = ref(true);
 const navigation = useNavigationStore();
-const rail = ref(false);
 const route = useRoute();
 const router = useRouter();
 const userStore = useUserStore();
@@ -115,19 +145,19 @@ const toast = useToast();
 const menuItems = [
     {
         title: 'Dashboard',
-        icon: 'space_dashboard',
+        icon: 'fluent:board-28-filled',
         to: '/dashboard',
         name: 'dashboard',
     },
     {
         title: 'Cartões',
-        icon: 'credit_card',
+        icon: 'fluent:card-ui-24-filled',
         to: '/cards',
         name: 'cards',
     },
     {
         title: 'Categorias',
-        icon: 'list_alt',
+        icon: 'fluent:apps-list-detail-24-filled',
         to: '/categories',
         name: 'categories',
     },
@@ -139,6 +169,8 @@ const showDrawer = computed(() => {
     ];
     return !excludedRoutesName.includes(route.name as string);
 });
+
+const getUserFirstName = computed(() => userStore.user?.name.split(' ')[0]);
 
 const isMenuActive = (name: string) => route.name === name;
 
@@ -152,4 +184,16 @@ async function logout() {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.box-avatar {
+    width: 80px;
+    height: 80px;
+    border: 2px solid #359766;
+    border-radius: 6px;
+
+    &.rail {
+        width: 48px;
+        height: 48px;
+    }
+}
+</style>
