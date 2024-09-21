35cab77f7a51338131074e1e42c6813b62d37712
dukmarques <eduardomarques@outlook.com>
2024-09-20 23:06:53 -0300

    feat: add floating button component

---
+++ src/components/form/FabButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-fab
+        v-bind="$attrs"
+        class="mr-8"
+        icon="mdi-plus"
+        location="top end"
+        absolute
+        offset
+        color="primary"
+        title="Adicionar"
+    >
+        <slot>
+            <Icon icon="fluent:add-24-filled" width="24" />
+        </slot>
+    </v-fab>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
