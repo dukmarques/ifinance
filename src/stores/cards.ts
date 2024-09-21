8c65551581247b2425c50fe26d067730eea60018
dukmarques <eduardomarques@outlook.com>
2024-09-20 23:08:07 -0300

    feat(stores): add loading state and fetch cards function on cards store

--- src/stores/cards.ts
+++ src/stores/cards.ts
@@ -5,21 +5,36 @@ import { axios } from '../services/axios';
 export const useCardsStore = defineStore('cardsStore', {
     state: () => ({
         cards: [] as Card[],
+        loading: false as boolean,
     }),
-    getters: {
-        getCards(): Card[] {
-            return this.cards;
-        },
-    },
+    getters: {},
     actions: {
         async fetchCards() {
+            this.fillCardsForLoading();
             // eslint-disable-next-line no-useless-catch
             try {
+                this.loading = true;
                 const { data } = await axios.get('/cards');
-                this.cards = data;
+                this.cards = data.data;
             } catch (err: any) {
                 throw err;
+            } finally {
+                this.loading = false;
             }
+        },
+
+        fillCardsForLoading() {
+            this.cards = Array(12).fill({
+                id: '1',
+                name: 'Cartão de Crédito',
+                closing_date: '',
+                due_date: '',
+                user_id: '123',
+            });
+        },
+
+        toggleLoading() {
+            this.loading = !this.loading;
         },
     },
 });
