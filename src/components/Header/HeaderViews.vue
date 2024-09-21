db84785fd0ec64ea3454e897cd58a2d0914bfd2b
dukmarques <eduardomarques@outlook.com>
2024-09-20 23:07:06 -0300

    feat: add HeaderViews component

---
+++ src/components/Header/HeaderViews.vue
@@ -0,0 +1,58 @@
+<template>
+    <div
+        id="header"
+        class="d-flex justify-space-between align-center mt-5 mb-3 ml-2"
+    >
+        <div class="content">
+            <h1 class="text-h3 font-weight-bold">
+                {{ title }}
+            </h1>
+            <p v-if="subtitle" class="text-h6">
+                {{ subtitle }}
+            </p>
+        </div>
+
+        <FabButton
+            v-if="hasFabButton"
+            :title="fabButtonTitle"
+            @click="fabButtonClick"
+        />
+    </div>
+    <v-divider class="mt-3" color="primary"></v-divider>
+</template>
+
+<script setup lang="ts">
+import FabButton from '../form/FabButton.vue';
+
+defineProps({
+    title: {
+        type: String,
+        defaut: '',
+    },
+    subtitle: {
+        type: String,
+        default: null,
+    },
+
+    hasFabButton: {
+        type: Boolean,
+        default: false,
+    },
+
+    fabButtonTitle: {
+        type: String,
+        default: 'Adicionar',
+    },
+
+    fabButtonClick: {
+        type: Function,
+        default: () => {},
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+#header {
+    width: 100%;
+}
+</style>
