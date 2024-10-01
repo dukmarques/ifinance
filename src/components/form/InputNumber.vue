afa705b7690075a11127b1f9656558f548de44fa
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:28:58 -0300

    feat: add InputNumber component for number input with masking and validation

---
+++ src/components/form/InputNumber.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-number-input
+        v-bind="$attrs"
+        v-model="number"
+        @update:model-value="$emit('update:modelValue', number)"
+        :controlVariant="controlVariant"
+        :label="label"
+        density="comfortable"
+        variant="outlined"
+        base-color="#A8A8B3"
+        color="primary"
+        inset
+        :rules="withRules ? rules : []"
+        :max="max"
+        :min="min"
+    ></v-number-input>
+</template>
+
+<script setup lang="ts">
+import { toRef, type PropType } from 'vue';
+
+
+const props = defineProps({
+    modelValue: Number,
+    label: {
+        type: String,
+        default: 'Informe o número',
+    },
+    controlVariant: {
+        type: String as PropType<string>,
+        default: 'split',
+    },
+    withRules: {
+        type: Boolean as PropType<boolean>,
+        default: false,
+    },
+    rules: {
+        type: Array as PropType<string[]>,
+        default: () => [(v: string) => !!v || 'Campo obrigatório'],
+    },
+    max: {
+        type: Number as PropType<number>,
+        default: 100,
+    },
+    min: {
+        type: Number as PropType<number>,
+        default: 100,
+    },
+});
+
+defineEmits(['update:modelValue']);
+
+const number = toRef(props.modelValue);
+</script>
