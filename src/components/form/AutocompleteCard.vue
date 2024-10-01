2138a95b06ea67eb10ec48e3d6d0cba304f5a85b
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:28:27 -0300

    feat: add AutocompleteCard component for credit card selection

---
+++ src/components/form/AutocompleteCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-autocomplete
+        v-model="item"
+        @update:model-value="$emit('update:modelValue', item)"
+        :items="items"
+        item-title="name"
+        item-value="name"
+        label="Select"
+        chips
+        closable-chips
+        density="comfortable"
+        variant="outlined"
+        base-color="#A8A8B3"
+        color="primary"
+    >
+        <template v-slot:chip="{ props, item }">
+            <v-chip
+                v-bind="props"
+                :prepend-avatar="item.raw.icon"
+                :text="item.raw.flag"
+            ></v-chip>
+        </template>
+
+        <template v-slot:item="{ props, item }">
+            <v-list-item
+                v-bind="props"
+                :prepend-avatar="item.raw.icon"
+                :title="item.raw.flag"
+            ></v-list-item>
+        </template>
+    </v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import { items } from '@/utils/creditIcons';
+import { toRef } from 'vue';
+
+const props = defineProps({
+    modelValue: String,
+});
+
+defineEmits(['update:modelValue']);
+
+const item = toRef(props.modelValue);
+
+</script>
