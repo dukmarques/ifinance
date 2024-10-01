4d3ef666465dac18da82086f2f3ea7a73fa968cf
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:27:33 -0300

    feat: add currency formatting helper function

---
+++ src/helpers/currencyFormat.ts
@@ -0,0 +1,8 @@
+const formatCurrency = (value: number): string => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(value);
+}
+
+export { formatCurrency };
