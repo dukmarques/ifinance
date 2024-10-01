e882663e37f0890c9757399a0dcedf68af7145b5
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:29:53 -0300

    feat: add credit card icons and utility functions

---
+++ src/utils/creditIcons.ts
@@ -0,0 +1,104 @@
+import pay99Icon from '@/assets/card-credit-icons/99-pay.png';
+import btgPactualIcon from '@/assets/card-credit-icons/btg-pactual.png';
+import interIcon from '@/assets/card-credit-icons/inter.png';
+import nubankIcon from '@/assets/card-credit-icons/nubank.png';
+import aleloIcon from '@/assets/card-credit-icons/alelo.png';
+import c6Icon from '@/assets/card-credit-icons/c6.png';
+import itauIcon from '@/assets/card-credit-icons/itau.png';
+import pagseguroIcon from '@/assets/card-credit-icons/pagseguro.png';
+import amazonIcon from '@/assets/card-credit-icons/amazon.png';
+import caixaIcon from '@/assets/card-credit-icons/caixa.png';
+import itiIcon from '@/assets/card-credit-icons/iti.png';
+import panIcon from '@/assets/card-credit-icons/pan.png';
+import amexIcon from '@/assets/card-credit-icons/amex.png';
+import cajuIcon from '@/assets/card-credit-icons/caju.png';
+import magaluIcon from '@/assets/card-credit-icons/magalu.png';
+import paypalIcon from '@/assets/card-credit-icons/paypal.png';
+import bancoDoBrasilIcon from '@/assets/card-credit-icons/banco-do-brasil.png';
+import credicardIcon from '@/assets/card-credit-icons/credicard.png';
+import mastercardIcon from '@/assets/card-credit-icons/mastercard.png';
+import picpayIcon from '@/assets/card-credit-icons/picpay.png';
+import banrisulIcon from '@/assets/card-credit-icons/banrisul.png';
+import digioIcon from '@/assets/card-credit-icons/digio.png';
+import mercadoPagoIcon from '@/assets/card-credit-icons/mercado-pago.png';
+import santanderIcon from '@/assets/card-credit-icons/santander.png';
+import binanceIcon from '@/assets/card-credit-icons/binance.png';
+import efiIcon from '@/assets/card-credit-icons/efi.png';
+import midwayIcon from '@/assets/card-credit-icons/midway.png';
+import visaIcon from '@/assets/card-credit-icons/visa.png';
+import bradescoIcon from '@/assets/card-credit-icons/bradesco.png';
+import eloIcon from '@/assets/card-credit-icons/elo.png';
+import neonIcon from '@/assets/card-credit-icons/neon.png';
+import willBankIcon from '@/assets/card-credit-icons/will-bank.png';
+
+const items = [
+    { flag: "Nubank", icon: nubankIcon, name: "nubankIcon" },
+    { flag: "Banco do Brasil", icon: bancoDoBrasilIcon, name: "bancoDoBrasilIcon" },
+    { flag: "Bradesco", icon: bradescoIcon, name: "bradescoIcon" },
+    { flag: "Caju", icon: cajuIcon, name: "cajuIcon" },
+    { flag: "Visa", icon: visaIcon, name: "visaIcon" },
+    { flag: "Mastercard", icon: mastercardIcon, name: "mastercardIcon" },
+    { flag: "Santander", icon: santanderIcon, name: "santanderIcon" },
+    { flag: "PicPay", icon: picpayIcon, name: "picpayIcon" },
+    { flag: "PayPal", icon: paypalIcon, name: "paypalIcon" },
+    { flag: "99 Pay", icon: pay99Icon, name: "pay99Icon" },
+    { flag: "BTG Pactual", icon: btgPactualIcon, name: "btgPactualIcon" },
+    { flag: "Inter", icon: interIcon, name: "interIcon" },
+    { flag: "Alelo", icon: aleloIcon, name: "aleloIcon" },
+    { flag: "C6", icon: c6Icon, name: "c6Icon" },
+    { flag: "Itaú", icon: itauIcon, name: "itauIcon" },
+    { flag: "PagSeguro", icon: pagseguroIcon, name: "pagseguroIcon" },
+    { flag: "Amazon", icon: amazonIcon, name: "amazonIcon" },
+    { flag: "Caixa", icon: caixaIcon, name: "caixaIcon" },
+    { flag: "Iti", icon: itiIcon, name: "itiIcon" },
+    { flag: "Pan", icon: panIcon, name: "panIcon" },
+    { flag: "Amex", icon: amexIcon, name: "amexIcon" },
+    { flag: "Magalu", icon: magaluIcon, name: "magaluIcon" },
+    { flag: "Credicard", icon: credicardIcon, name: "credicardIcon" },
+    { flag: "Banrisul", icon: banrisulIcon, name: "banrisulIcon" },
+    { flag: "Digio", icon: digioIcon, name: "digioIcon" },
+    { flag: "Mercado Pago", icon: mercadoPagoIcon, name: "mercadoPagoIcon" },
+    { flag: "Binance", icon: binanceIcon, name: "binanceIcon" },
+    { flag: "Efi", icon: efiIcon, name: "efiIcon" },
+    { flag: "Midway", icon: midwayIcon, name: "midwayIcon" },
+    { flag: "Elo", icon: eloIcon, name: "eloIcon" },
+    { flag: "Neon", icon: neonIcon, name: "neonIcon" },
+    { flag: "Will Bank", icon: willBankIcon, name: "willBankIcon" }
+];
+
+const icons = {
+    pay99Icon,
+    btgPactualIcon,
+    interIcon,
+    nubankIcon,
+    aleloIcon,
+    c6Icon,
+    itauIcon,
+    pagseguroIcon,
+    amazonIcon,
+    caixaIcon,
+    itiIcon,
+    panIcon,
+    amexIcon,
+    cajuIcon,
+    magaluIcon,
+    paypalIcon,
+    bancoDoBrasilIcon,
+    credicardIcon,
+    mastercardIcon,
+    picpayIcon,
+    banrisulIcon,
+    digioIcon,
+    mercadoPagoIcon,
+    santanderIcon,
+    binanceIcon,
+    efiIcon,
+    midwayIcon,
+    visaIcon,
+    bradescoIcon,
+    eloIcon,
+    neonIcon,
+    willBankIcon,
+};
+
+export { items, icons };
