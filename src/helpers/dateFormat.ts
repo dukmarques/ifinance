f0f928e8032a680f1b2f9967944fdcac09d1f988
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:27:45 -0300

    feat: add error handling to formatDateToMMYY helper function

--- src/helpers/dateFormat.ts
+++ src/helpers/dateFormat.ts
@@ -1,9 +1,17 @@
 const formatDateToMMYY = (date: Date | string): string => {
-    const dateRaw = typeof date === 'string' ? new Date(date) : date;
-    return new Intl.DateTimeFormat('pt-BR', {
-        month: '2-digit',
-        year: '2-digit',
-    }).format(dateRaw);
+    if (typeof date !== "string" && !(date instanceof Date)) {
+        return '';
+    }
+
+    try {
+        const dateRaw = typeof date === 'string' ? new Date(date) : date;
+        return new Intl.DateTimeFormat('pt-BR', {
+            month: '2-digit',
+            year: '2-digit',
+        }).format(dateRaw);
+    } catch (error) {
+        return '';
+    }
 };
 
 export { formatDateToMMYY };
