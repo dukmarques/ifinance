3c658e85ba7b48d72e5bf74dcd4d5c3e10b25833
dukmarques <eduardomarques@outlook.com>
2024-09-30 23:29:33 -0300

    refactor: update Card type to use number for closing_date, due_date, and limit

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -3,9 +3,12 @@ import type { User } from './User';
 type Card = {
     id: string;
     name: string;
-    closing_date: string;
-    due_date: string;
+    closing_date: number;
+    due_date: number;
+    limit: number;
     user_id: string;
+    background_color?: string;
+    card_flag?: string;
     user?: User;
     card_expenses?: any[];
     card_expenses_count?: number;
