f173174e8fbebb8e9985882df87d28f8bf4de16d
dukmarques <eduardomarques@outlook.com>
2024-10-01 23:16:48 -0300

    refactor(CategoriesView): update template and script setup

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,3 +1,58 @@
-<template></template>
+<template>
+    <v-container
+        fluid
+        class="ml-5"
+    >
+        <v-row>
+            <HeaderViews
+                title="Categorias"
+                subtitle="Aqui estão todas as suas categorias"
+                hasFabButton
+                fabButtonTitle="Adicionar categoria"
+                :fabButtonClick="toggleCreateDialog"
+            />
+        </v-row>
 
-<script setup lang="ts"></script>
+        <v-row>
+            <v-col
+                v-for="category in categories"
+                :key="category.id"
+                cols="12"
+            >
+                {{ category }}
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useCategoriesStore } from '@/stores/categories';
+import type { Category } from '@/types/Category';
+
+import HeaderViews from '@/components/Header/HeaderViews.vue';
+
+const { categories, loading } = storeToRefs(useCategoriesStore());
+const { fetchCategories, createCategory } = useCategoriesStore();
+
+const createDialog = ref(false);
+const loadingDialog = ref(false);
+
+const toggleCreateDialog = () => {
+    createDialog.value = !createDialog.value;
+}
+
+fetchCategories();
+
+async function create(category: Category) {
+    loadingDialog.value = true;
+
+    try {
+        await createCategory(category);
+        createDialog.value = false;
+    } finally {
+        loadingDialog.value = false;
+    }
+}
+</script>
