bbbf2208896fbc4c398260d71d9c053d29c58f87
dukmarques <eduardomarques@outlook.com>
2025-02-05 22:44:10 -0300

    fix(App.vue): change overflow-y to auto

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,11 @@ import { RouterView } from 'vue-router';
 import NavigationDrawer from './components/Navigation/NavigationDrawer.vue';
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+html {
+    overflow-y: auto !important;
+}
+
 .Toastify__toast-container {
     width: 600px !important;
 }
