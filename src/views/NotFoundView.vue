f89b3633f1002f0be0686ffc5e56cfb11f027f6c
dukmarques <eduardomarques@outlook.com>
2025-02-19 23:53:29 -0300

    feat(NotFoundView): add custom 404 page with empty state and logo

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,17 @@
 <template>
+    <v-empty-state
+        headline="Ops, 404!"
+        title="Página não encontrada"
+        text="A página que você está tentando acessar não existe."
+        :image="logoSoloImg"
+        size="400"
+    >
+    </v-empty-state>
     <v-container>
         <h1>Página não encontrada</h1>
     </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logoSoloImg from '@/assets/logo-solo.png';
+</script>
