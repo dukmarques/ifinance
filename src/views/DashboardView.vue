364c4fa43a91acc9f736ecb54244e9fba913d107
dukmarques <eduardomarques@outlook.com>
2025-04-08 23:31:15 -0300

    feat(DashboardView): add layout structure with Header and Summary component

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,17 @@
 <template>
-    <VContainer></VContainer>
+    <VContainer>
+        <v-row>
+            <HeaderViews 
+                title="Overview" 
+                subtitle="Resumo dos seus gastos"
+            />
+        </v-row>
+
+        <Summary />
+    </VContainer>
 </template>
+
+<script setup lang="ts">
+import HeaderViews from '@/components/Header/HeaderViews.vue';
+import Summary from '@/components/Summary/SummaryFinance.vue';
+</script>
