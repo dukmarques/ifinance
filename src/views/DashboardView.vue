cd2ef4d7120725a76c88f6596cd2269314898723
dukmarques <eduardomarques@outlook.com>
2025-04-16 00:54:05 -0300

    feat(DashboardView): adicionar MonthSelector e ajustar espaçamento do v-divider

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,9 @@
 <template>
     <VContainer fluid>
+        <MonthSelector @month-change="handleMonthChange"/>
         <Summary />
 
-        <v-divider thickness="1" class="mt-7 mb-7"/>
+        <v-divider class="mt-7 mb-7 mx-auto" thickness="1" />
 
         <ExpasionPanels />
         <ExpasionPanels />
@@ -14,6 +15,16 @@
 
 <script setup lang="ts">
 import ExpasionPanels from '@/components/Dashboard/ExpasionPanels.vue';
+import MonthSelector from '@/components/Summary/MonthSelector.vue';
 import Summary from '@/components/Summary/SummaryFinance.vue';
 import FabSpeedDial from '@/components/form/FabSpeedDial.vue';
+
+import { ref } from 'vue';
+
+const currentMonth = ref('');
+
+function handleMonthChange(date: string) {
+    currentMonth.value = date;
+    console.log('Selected month:', date);
+}
 </script>
