7c02c3c88b77545bcf63af38eb8305c0932523be
dukmarques <eduardomarques@outlook.com>
2025-04-30 01:06:47 -0300

    feat(InputDatePicker): add new component

---
+++ src/components/form/InputDatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-date-input
+        v-model="model"
+        @update:model-value="$emit('update:modelValue', model)"
+        clearable 
+        :label="label" 
+        variant="outlined"
+        prepend-icon=""
+        :display-format="format"
+        density="comfortable"
+        divided
+        show-adjacent-months
+    ></v-date-input>
+</template>
+
+<script lang="ts" setup>
+import { shallowRef } from 'vue';
+
+const props = defineProps({
+    modelValue: {
+        type: [
+            Date, String
+        ],
+    },
+    label: {
+        type: String,
+        default: 'Data',
+    },
+});
+
+defineEmits(['update:modelValue']);
+
+const model = shallowRef(props.modelValue);
+
+function format(date: Date) {
+    return date.toLocaleDateString('pt-BR');
+}
+</script>
