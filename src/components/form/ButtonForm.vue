50acbff778be685565f4f1d3baee2e67f4a78899
dukmarques <eduardomarques@outlook.com>
2025-04-30 01:08:21 -0300

    feat(ButtonForm): add support for dynamic attributes and keep slot for title

--- src/components/form/ButtonForm.vue
+++ src/components/form/ButtonForm.vue
@@ -1,12 +1,22 @@
 <template>
     <v-btn
-        v-bind="$attrs"
+        class="text-none text-subtitle-1"
         type="submit"
         color="primary"
-        dark
-        density="default"
+        density="compact"
         size="large"
     >
-        <slot></slot>
+        <slot>
+            <span>{{ title }}</span>
+        </slot>
     </v-btn>
 </template>
+
+<script lang="ts" setup>
+defineProps({
+    title: {
+        type: String,
+        default: 'Salvar',
+    },
+})
+</script>
