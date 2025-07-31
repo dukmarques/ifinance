f00ad61c7a2126b46c8f48ca8dfe0ef9062b0862
Eduardo Marques <eduardomarques@outlook.com>
2025-07-31 00:43:24 -0300

    feat(FabButton): migrate to PrimeVue Button component and enhance tooltip functionality

--- src/components/form/FabButton.vue
+++ src/components/form/FabButton.vue
@@ -1,20 +1,35 @@
-<template>
-    <v-fab
-        v-bind="$attrs"
-        class="mr-8"
-        icon="mdi-plus"
-        location="top end"
-        absolute
-        offset
-        color="primary"
-        title="Adicionar"
-    >
-        <slot>
-            <v-icon icon="fa-solid fa-plus" color="white" size="small"/>
-        </slot>
-    </v-fab>
-</template>
+<script lang="ts" setup>
+import Button from 'primevue/button';
+import { defineProps } from 'vue';
 
-<script setup lang="ts">
+type FabButtonProps = {
+    title?: string;
+    location?: 'top end' | 'top start' | 'bottom end' | 'bottom start';
+};
 
+withDefaults(defineProps<FabButtonProps>(), {
+    title: 'Adicionar',
+    location: 'top end',
+});
 </script>
+
+<template>
+    <Button  
+        icon="pi pi-plus"
+        icon-class="text-gray-200"
+        rounded 
+        :aria-label="title"
+        v-tooltip.top="{
+            value: title || 'Adicionar',
+            showDelay: 1000,
+        }" 
+        size="large"
+        :class="[
+            '!absolute z-10 !bg-primary hover:brightness-[1.3] transition-all duration-300 border-0',
+            location === 'top end' ? 'top-8 right-8' : '',
+            location === 'top start' ? 'top-8 left-8' : '',
+            location === 'bottom end' ? 'bottom-8 right-8' : '',
+            location === 'bottom start' ? 'bottom-8 left-8' : '',
+        ]"
+    />
+</template>
