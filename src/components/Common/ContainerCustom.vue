0757151c01d059c50eed5a1669e130bd4212b07a
Eduardo Marques <eduardomarques@outlook.com>
2025-07-31 00:43:48 -0300

    feat(ContainerCustom): add new component for flexible layout with customizable properties

---
+++ src/components/Common/ContainerCustom.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    fluid: {
+        type: Boolean,
+        default: false,
+    },
+    tag: {
+        type: String,
+        default: 'div'
+    },
+    padding: {
+        type: Boolean,
+        default: true
+    },
+    maxWidth: {
+        type: String,
+        default: ''
+    }
+});
+</script>
+
+<template>
+    <component 
+        :is="tag" 
+        :class="[
+            fluid ? 'w-full' : 'container',
+            'mx-auto',
+            padding ? 'px-4 sm:px-6 md:px-8' : '',
+            maxWidth
+        ]"
+    >
+        <slot></slot>
+    </component>
+</template>
