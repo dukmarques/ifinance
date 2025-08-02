ae76dbf17286ca9edd6b763cb596e7daf02ac712
Eduardo Marques <eduardomarques@outlook.com>
2025-08-02 00:23:39 -0300

    feat(ToggleDarkMode): add toggle dark mode component

---
+++ src/components/Common/ToggleDarkMode.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useNavigationStore } from '@/stores/navigation';
+
+const navigation = useNavigationStore();
+</script>
+
+<template>
+    <i 
+        v-tooltip.bottom="'Alterar tema'"
+        class="pi pi-sun cursor-pointer" 
+        :class="[
+            'pi cursor-pointer',
+            navigation.isDarkMode ? 'pi-moon' : 'pi-sun',
+        ]"
+        @click="navigation.toggleDarkMode"
+    ></i>
+</template>
