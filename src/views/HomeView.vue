99f8fa6476ba2ddc4b30c13ec26b643b0596ad5b
Eduardo Marques <eduardomarques@outlook.com>
2025-08-02 00:21:37 -0300

    refactor(HomeView): reorganize code and improve component structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import logoImg from '@/assets/logo-name-primary.png';
+import BaseButton from '@/components/BaseForm/BaseButton.vue';
+import Image from 'primevue/image';
+import ToggleDarkMode from '@/components/Common/ToggleDarkMode.vue';
+
+const router = useRouter();
+</script>
+
 <template>
-    <div class="flex justify-center items-center h-screen">
+    <div class="w-full h-screen flex justify-center items-center relative">
         <div class="flex flex-col justify-center items-center gap-10">
             <Image 
                 :src="logoImg" 
@@ -16,16 +26,10 @@
                 class="bg-primary text-white w-[245px] h-[50px] py-3 rounded-lg shadow-md "
                 @click="router.push('/login')"
             />
+
+            <div class="absolute top-4 right-4">
+                <ToggleDarkMode />
+            </div>
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-import { useRouter } from 'vue-router';
-import logoImg from '@/assets/logo-name-primary.png';
-import BaseButton from '@/components/BaseForm/BaseButton.vue';
-import Image from 'primevue/image';
-
-
-const router = useRouter();
-</script>
