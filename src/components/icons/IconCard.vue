8948c09ff2ad117d4388a3d07367b71ae27d972b
Eduardo Marques <eduardomarques@outlook.com>
2025-08-02 00:22:21 -0300

    feat(Icons): add dynamic color support to icons

--- src/components/icons/IconCard.vue
+++ src/components/icons/IconCard.vue
@@ -1,6 +1,27 @@
+<script lang="ts" setup>
+defineProps({
+    color: {
+        type: String,
+        default: 'white',
+    },
+})
+</script>
+
 <template>
     <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M22.1819 6H5.81819C4.81403 6 4 6.89543 4 8V20C4 21.1045 4.81403 22 5.81819 22H22.1819C23.186 22 24 21.1045 24 20V8C24 6.89543 23.186 6 22.1819 6Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M4 11H24" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path 
+            d="M22.1819 6H5.81819C4.81403 6 4 6.89543 4 8V20C4 21.1045 4.81403 22 5.81819 22H22.1819C23.186 22 24 21.1045 24 20V8C24 6.89543 23.186 6 22.1819 6Z" 
+            :stroke="color" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+        />
+        <path 
+            d="M4 11H24" 
+            :stroke="color" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+        />
     </svg>
 </template>
