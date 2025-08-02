148f33465d085e2f09ededf34c39ffc5417846fc
Eduardo Marques <eduardomarques@outlook.com>
2025-08-02 00:24:06 -0300

    feat(NavigationStore): add dark mode toggle functionality

--- src/stores/navigation.ts
+++ src/stores/navigation.ts
@@ -3,16 +3,31 @@ import { defineStore } from 'pinia';
 export const useNavigationStore = defineStore('navigationStore', {
     state: () => ({
         rail: false,
+        darkMode: false,
     }),
     persist: true,
     getters: {
         getRail(): boolean {
             return this.rail;
         },
+        isDarkMode(): boolean {
+            return this.darkMode;
+        }
     },
     actions: {
         toggleRail() {
             this.rail = !this.rail;
         },
+        toggleDarkMode() {
+            this.darkMode = !this.darkMode;
+            document.documentElement.classList.toggle('active-dark-mode');
+        },
+        initializeDarkMode() {
+            if (this.darkMode) {
+                document.documentElement.classList.add('active-dark-mode');
+            } else {
+                document.documentElement.classList.remove('active-dark-mode');
+            }
+        }
     },
 });
