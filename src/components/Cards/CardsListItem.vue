ffcc623eaafbb1faf01611c1899ccdb0d570736a
dukmarques <eduardomarques@outlook.com>
2025-08-09 00:12:23 -0300

    feat(Cards): enhance card deletion confirmation and improve UI elements

--- src/components/Cards/CardsListItem.vue
+++ src/components/Cards/CardsListItem.vue
@@ -10,26 +10,32 @@ import { CountUp } from 'countup.js';
 import Menu from 'primevue/menu';
 import type { MenuItem } from 'primevue/menuitem';
 import { useConfirm } from 'primevue/useconfirm';
+import { getContrastColor } from '@/utils/colorUtils';
+import { useCardsStore } from '@/stores/cards';
+import { useToast } from '@/composables/useToast';
 
 const props = defineProps({
     card: {
         type: Object as PropType<CreditCard>,
         required: true,
     },
-    loading: {
-        type: Boolean,
-        default: false,
-    },
 });
+
+const { updateCard, deleteCard } = useCardsStore();
+const { showSuccess, showError } = useToast();
 
 const cardFlagSrc = computed(() => {
     const cardIcon = props.card.card_flag as keyof typeof icons ?? 'defaultCard';
     return icons[cardIcon];
-})
+});
+
+const textColor = computed(() => {
+    return getContrastColor(props.card.background_color || '#FFFFFF');
+});
 
 const confirmDialog = useConfirm();
 const menu = ref();
-const menuOptions = ref<MenuItem>([
+const menuOptions = ref<MenuItem[]>([
     {
         label: 'Editar',
         icon: 'pi pi-pencil',
@@ -41,16 +47,24 @@ const menuOptions = ref<MenuItem>([
         icon: 'pi pi-trash',
         command: () => {
             confirmDialog.require({
-                message: 'Você tem certeza que deseja remover este cartão?',
-                header: 'Confirmação',
-                acceptLabel: 'Remover',
+                message: 'Você tem certeza que deseja excluir este cartão?',
+                header: `Excluir cartão: ${props.card.name}`,
+                acceptLabel: 'Excluir',
                 rejectLabel: 'Cancelar',
                 acceptClass: 'p-button-danger',
-                accept: () => deleteCard(props.card.id),
+                acceptProps: {
+                    icon: 'pi pi-trash',
+                },
+                accept: () => deleteCard(props.card.id)
+                    .then(() => {
+                        showSuccess({ message: 'Sucesso!', description: 'Cartão excluído com sucesso' });
+                    }).catch((err: any) => {
+                        showError({ message: 'Erro ao excluir cartão', description: err.response?.data?.message });
+                    }),
             });
         }
     }
-])
+]);
 
 const toggleCardOptions = (event: Event) => {
     menu.value.toggle(event);
@@ -58,7 +72,6 @@ const toggleCardOptions = (event: Event) => {
 
 
 const countUp = ref<CountUp | null>(null);
-
 onMounted(() => {
     const elementId = `available-${props.card.id}`;
 
@@ -82,14 +95,15 @@ onMounted(() => {
 <template>
     <ConfirmPopup group="card-options" />
     <div 
-        class="w-full h-[230px] rounded-2xl bg-cover bg-position-[-8px] !p-5 relative" 
+        class="w-full h-[230px] flex flex-col justify-center rounded-2xl bg-cover bg-position-[-8px] !p-5 relative" 
         :style="`
             background-color: ${props.card.background_color!}; 
             background-image: url(${CreditCardBg});
+            color: ${textColor};
         `"
     >
         <div class="flex justify-between items-center">
-            <p class="text-sm text-white">{{ props.card.name }}</p>
+            <p class="text-2xl font-bold">{{ props.card.name }}</p>
             <BaseImage 
                 :src="cardFlagSrc"
                 width="40"
@@ -98,24 +112,20 @@ onMounted(() => {
             />
         </div>
 
-        <div class="flex flex-col gap-2">
-            <p class="text-sm text-white">Limite disponível</p>
-            <p :id="`available-${props.card.id}`">0</p>
-            <div class="flex justify-between">
+        <div class="flex flex-col gap-0 mt-2">
+            <p class="text-lg">Limite disponível</p>
+            <p :id="`available-${props.card.id}`" class="text-lg font-bold">0</p>
+            <div class="flex justify-between mt-2">
                 <p>R$0</p>
                 <p>{{ formatCurrency(props.card.limit) }}</p>
             </div>
             <ProgressBar 
-                class="!h-1" 
+                class="!h-1 mt-1" 
                 :showValue="false" 
                 :value="70" 
                 :pt="{
-                    root: {
-                        class: '!bg-white/40 rounded-full',
-                    },
-                    value: {
-                        class: '!bg-white rounded-full',
-                    },
+                    root: { class: textColor === '#000000' ? '!bg-black/20 rounded-full' : '!bg-white/40 rounded-full' },
+                    value: { class: textColor === '#000000' ? '!bg-black/60 rounded-full' : '!bg-white rounded-full' },
                 }"
             />
         </div>
@@ -129,7 +139,7 @@ onMounted(() => {
             class="absolute top-2 right-2 cursor-pointer"
             @click="toggleCardOptions"
         >
-            <i class="pi pi-ellipsis-v"></i>
+            <i class="pi pi-ellipsis-v" :style="{ color: textColor }"></i>
             <Menu 
                 ref="menu" 
                 id="overlay_menu" 
