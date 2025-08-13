b020fe14c3c37657d9a4a4ee3e7b3b4660fa1dd5
dukmarques <eduardomarques@outlook.com>
2025-08-12 22:23:52 -0300

    refactor(SelectCardsTags): reorganize template structure to include InputGroup and InputGroupAddon for better layout

--- src/components/Cards/SelectCardsTags.vue
+++ src/components/Cards/SelectCardsTags.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { items } from '@/utils/creditIcons';
 import Select from 'primevue/select';
 import FloatLabel from 'primevue/floatlabel';
 import Message from 'primevue/message';
-import { items } from '@/utils/creditIcons';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
 
 export interface SelectCardsTagsProps {
     name: string;
@@ -27,49 +29,54 @@ function getItemByName(name: string) {
 </script>
 
 <template>
-    <FloatLabel class="w-full !h-12" variant="on">
-        <Select 
-            :name="name"
-            :options="cardItems" 
-            filter 
-            class="!h-full"
-            optionLabel="flag"
-            optionValue="name"
-            :disabled="disabled"
-            fluid
-        >
-            <template #value="slotProps">
-                <div 
-                    v-if="slotProps.value" 
-                    class="flex items-center"
-                >
-                    <img 
-                        :alt="getItemByName(slotProps.value)?.flag || 'Cartão'" 
-                        :src="getItemByName(slotProps.value)?.icon" 
-                        class="mr-2" 
-                        style="width: 24px" 
-                    />
-                    <div>{{ getItemByName(slotProps.value)?.flag || slotProps.value }}</div>
-                </div>
-                <span v-else>
-                    {{ slotProps.placeholder }}
-                </span>
-            </template>
-            <template #option="slotProps">
-                <div class="flex items-center">
-                    <img 
-                        :alt="slotProps.option.flag" 
-                        :src="slotProps.option.icon" 
-                        class="mr-2" 
-                        style="width: 24px" 
-                    />
-                    <div>{{ slotProps.option.flag }}</div>
-                </div>
-            </template>
-        </Select>
-
-        <label v-if="label" :for="name">{{ label }}</label>
-
-        <Message v-if="invalid && errorMessage" severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
-    </FloatLabel>
+    <InputGroup>
+        <InputGroupAddon>
+            <i class="pi pi-credit-card"></i>
+        </InputGroupAddon>
+        <FloatLabel class="w-full !h-12" variant="on">
+            <Select 
+                :name="name"
+                :options="cardItems" 
+                filter 
+                class="!h-full"
+                optionLabel="flag"
+                optionValue="name"
+                :disabled="disabled"
+                fluid
+            >
+                <template #value="slotProps">
+                    <div 
+                        v-if="slotProps.value" 
+                        class="flex items-center mt-1"
+                    >
+                        <img 
+                            :alt="getItemByName(slotProps.value)?.flag || 'Cartão'" 
+                            :src="getItemByName(slotProps.value)?.icon" 
+                            class="mr-2" 
+                            style="width: 24px" 
+                        />
+                        <div>{{ getItemByName(slotProps.value)?.flag || slotProps.value }}</div>
+                    </div>
+                    <span v-else>
+                        {{ slotProps.placeholder }}
+                    </span>
+                </template>
+                <template #option="slotProps">
+                    <div class="flex items-center">
+                        <img 
+                            :alt="slotProps.option.flag" 
+                            :src="slotProps.option.icon" 
+                            class="mr-2" 
+                            style="width: 24px" 
+                        />
+                        <div>{{ slotProps.option.flag }}</div>
+                    </div>
+                </template>
+            </Select>
+    
+            <label v-if="label" :for="name">{{ label }}</label>
+    
+            <Message v-if="invalid && errorMessage" severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
+        </FloatLabel>
+    </InputGroup>
 </template>
