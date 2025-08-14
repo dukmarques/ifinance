8585570227a77e1ed4d59ce929b0ed6f872c0393
dukmarques <eduardomarques@outlook.com>
2025-08-14 00:20:23 -0300

    feat(BaseFabSpeedDial): add new component with customizable speed dial options

---
+++ src/components/BaseForm/BaseFabSpeedDial.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { MenuItem } from 'primevue/menuitem';
+import SpeedDial from 'primevue/speeddial';
+
+export interface BaseFabSpeedDialProps {
+    items: MenuItem[];
+    direction?: 'up' | 'down' | 'left' | 'right' | 'up-left' | 'up-right' | 'down-left' | 'down-right';
+    type?: 'linear' | 'circle' | 'semi-circle' | 'quarter-circle';
+    radius?: number;
+    tooltipOptions?: {
+        event?: 'hover' | 'focus';
+        position?: 'bottom' | 'top' | 'left' | 'right';
+    };
+}
+
+withDefaults(defineProps<BaseFabSpeedDialProps>(), {
+    direction: 'up',
+    type: 'linear',
+    radius: 80,
+    tooltipOptions: () => ({
+        event: 'hover',
+        position: 'bottom'
+    }),
+});
+</script>
+
+<template>
+    <SpeedDial 
+        :model="items" 
+        :direction="direction" 
+        :type="type"
+        :radius="radius"
+        :tooltipOptions="{ 
+            event: tooltipOptions.event, 
+            position: tooltipOptions.position 
+        }"
+    />
+</template>
