596e2f908ee4b3dfb96b49ee33bfb58eae0e2272
dukmarques <eduardomarques@outlook.com>
2025-08-18 23:49:44 -0300

    feat(BaseToggleSwitch): add new component for toggle switch input with error message display

---
+++ src/components/BaseForm/BaseToggleSwitch.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import ToggleSwitch from 'primevue/toggleswitch';
+import Message from 'primevue/message';
+
+export interface BaseInputToggleSwitchProps {
+    label?: string;
+    name: string;
+    disabled?: boolean;
+    invalid?: boolean;
+    errorMessage?: string;
+}
+
+withDefaults(defineProps<BaseInputToggleSwitchProps>(), {
+    disabled: false,
+    invalid: false,
+});
+</script>
+
+<template>
+    <div class="w-full flex gap-2">
+        <ToggleSwitch 
+            :name="name"
+            :disabled="disabled"
+            :invalid="invalid"
+        />
+        <label v-if="label" :for="name">{{ label }}</label>
+        <Message 
+            severity="error" 
+            size="small" 
+            variant="simple"
+        >
+            {{ errorMessage }}
+        </Message>
+    </div>
+</template>
