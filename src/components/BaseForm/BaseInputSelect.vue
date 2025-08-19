15f243a672c8f9ae8af2f466a9063a699cd85a3c
dukmarques <eduardomarques@outlook.com>
2025-08-18 23:49:59 -0300

    feat(BaseInputSelect): add new component for select input with error message display

---
+++ src/components/BaseForm/BaseInputSelect.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import Select from 'primevue/select';
+import Message from 'primevue/message';
+import FloatLabel from 'primevue/floatlabel';
+
+export interface BaseInputBaseInputSelectProps {
+    label?: string;
+    name: string;
+    items: Array<{ name: string; value: string }>;
+    size?: 'small' | 'large';
+    disabled?: boolean;
+    invalid?: boolean;
+    errorMessage?: string;
+    showPrefixIcon?: boolean;
+    prefixIcon?: string;
+}
+
+withDefaults(defineProps<BaseInputBaseInputSelectProps>(), {
+    size: 'large',
+    disabled: false,
+    showPrefixIcon: false,
+    prefixIcon: 'pi-tag',
+});
+</script>
+
+<template>
+    <InputGroup>
+        <InputGroupAddon v-if="showPrefixIcon">
+            <i :class="['pi', prefixIcon]" />
+        </InputGroupAddon>
+        <FloatLabel variant="on">
+            <Select 
+                :id="name"
+                :name="name"
+                :options="items"
+                optionLabel="name"
+                optionValue="value"
+                :size="size"
+                :disabled="disabled"
+                :invalid="invalid"
+                filter 
+                fluid
+            />
+    
+            <label v-if="label" :for="name">{{ label }}</label>
+    
+        </FloatLabel>
+    </InputGroup>
+    <Message severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
+</template>
