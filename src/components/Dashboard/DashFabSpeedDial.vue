4708eba7ef13c7a1e359698bd4ca8984134d4977
dukmarques <eduardomarques@outlook.com>
2025-08-18 23:50:13 -0300

    feat(DashFabSpeedDial): add new component for revenue management with speed dial functionality

---
+++ src/components/Dashboard/DashFabSpeedDial.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+import { ref, useTemplateRef } from 'vue';
+import type { MenuItem } from 'primevue/menuitem';
+import type { Revenues } from '@/@types/Revenues';
+import { useRevenuesStore } from '@/stores/revenues';
+import { useToast } from '@/composables/useToast';
+import BaseFabSpeedDial from '../BaseForm/BaseFabSpeedDial.vue';
+import ManageRevenuesDialogNew from '@/components/Revenues/ManageRevenuesDialogNew.vue';
+
+const createRevenueDialogRef = useTemplateRef<InstanceType<typeof ManageRevenuesDialogNew>>('createRevenueDialog');
+
+const items = ref<MenuItem[]>([
+    {
+        label: 'Adicionar entrada',
+        icon: 'pi pi-arrow-up',
+        command: () => createRevenueDialogRef.value!.visible = true,
+    },
+    {
+        label: 'Adicionar saída',
+        icon: 'pi pi-arrow-down-right',
+        command: () => console.log('Adicionar saída'),
+    },
+    {
+        label: 'Adicionar saída de cartão',
+        icon: 'pi pi-credit-card',
+        command: () => console.log('Adicionar saída de cartão'),
+    },
+]);
+
+const { showSuccess, showError } = useToast();
+const loadingCreateRevenue = ref(false);
+const revenueStore = useRevenuesStore();
+
+async function handleCreateRevenue(revenue: Revenues) {
+    try {
+        loadingCreateRevenue.value = true;
+        await revenueStore.create(revenue);
+        showSuccess({
+            message: 'Sucesso!',
+            description: 'Receita adicionada com sucesso.'
+        })
+    } catch (err) {
+        const error = err as Error;
+        showError({
+            message: 'Ocorreu um erro!',
+            description: error.message || 'Erro ao adicionar receita.'
+        })
+    } finally {
+        loadingCreateRevenue.value = false;
+    }
+}
+</script>
+
+<template>
+    <BaseFabSpeedDial
+        :items="items"
+        direction="up-left"
+        type="quarter-circle"
+        :radius="70"
+        class="!fixed bottom-5 right-3"
+    />
+
+    <ManageRevenuesDialogNew 
+        ref="createRevenueDialog"
+        :loading="loadingCreateRevenue"
+        :provider="handleCreateRevenue"
+    />
+</template>
