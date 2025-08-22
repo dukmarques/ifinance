686228256cebd70202e3448d6299f79b12116825
dukmarques <eduardomarques@outlook.com>
2025-08-22 00:03:35 -0300

    refactor(RevenuesList): remove unused imports and simplify component structure

--- src/components/Revenues/RevenuesList.vue
+++ src/components/Revenues/RevenuesList.vue
@@ -1,19 +1,14 @@
 <script lang="ts" setup>
-import { ref, toRef, useTemplateRef, watch } from 'vue';
+import { toRef, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useRevenuesStore } from '@/stores/revenues';
 import { formatCurrency } from '@/helpers/currencyFormat';
-import type { Revenues } from '@/@types/Revenues';
 
 import Accordion from 'primevue/accordion';
 import AccordionPanel from 'primevue/accordionpanel';
 import AccordionHeader from 'primevue/accordionheader';
 import AccordionContent from 'primevue/accordioncontent';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ModificationsTypeDialog, { type ModificationsTypeItem } from '../Common/ModificationsTypeDialog.vue';
-import { useConfirm } from 'primevue/useconfirm';
-import { useToast } from '@/composables/useToast';
+import RevenuesListItems from '@/components/Revenues/RevenuesListItems.vue';
 
 const props = defineProps({
     date: {
@@ -25,9 +20,8 @@ const props = defineProps({
     },
 });
 
-const { revenues, loading, getTotalRevenuesAmount } = storeToRefs(useRevenuesStore());
-const { fetchRevenues, update, delete: deleteRevenue } = useRevenuesStore();
-const { showSuccess, showError } = useToast();
+const { getTotalRevenuesAmount } = storeToRefs(useRevenuesStore());
+const { fetchRevenues } = useRevenuesStore();
 
 const selectedDate = toRef(props, 'date');
 watch(
@@ -36,62 +30,6 @@ watch(
         fetchRevenues(newDate);
     },
 );
-
-const expandedRows = ref({});
-
-const modificationsDialogRef = useTemplateRef<InstanceType<typeof ModificationsTypeDialog>>('modificationsDialog');
-const selectedRevenue = ref<Revenues | null>(null);
-const confirmDialog = useConfirm();
-
-async function onEdit(revenue: Revenues) {
-    // TODO: em receitas recorrentes com deprecated_date, exibir o input do deprecated_date,
-    // para caso usuário deseje alterar até quando a receita será recorrente
-    console.log('Editing revenue:', revenue);
-}
-
-
-async function onDelete(revenue: Revenues) {
-    if (revenue.recurrent) {
-        selectedRevenue.value = revenue;
-        modificationsDialogRef.value!.show();
-        return;
-    }
-
-    confirmDialog.require({
-        message: 'Você tem certeza que deseja excluir esta receita?',
-        header: `Excluir receita: ${revenue.title}`,
-        acceptLabel: 'Excluir',
-        rejectLabel: 'Cancelar',
-        acceptClass: 'p-button-danger',
-        acceptProps: {
-            icon: 'pi pi-trash',
-        },
-        accept: () => deleteRevenue(revenue.id)
-            .then(() => {
-                showSuccess({ message: 'Sucesso!', description: 'Receita excluída com sucesso' });
-            }).catch((err: any) => {
-                showError({ message: 'Erro ao excluir receita', description: err.response?.data?.message });
-            }),
-    });
-}
-
-const modificationsOptions = ref<Array<ModificationsTypeItem>>([
-    {
-        title: 'Apenas atual',
-        severity: 'info',
-        onClick: () => deleteRevenue(selectedRevenue.value!.id, 'only_month'),
-    },
-    {
-        title: 'Atual e próximos meses',
-        severity: 'warn',
-        onClick: () => deleteRevenue(selectedRevenue.value!.id, 'current_month_and_followers'),
-    },
-    {
-        title: 'Todos os meses',
-        severity: 'danger',
-        onClick: () => deleteRevenue(selectedRevenue.value!.id, 'all_month'),
-    },
-]);
 </script>
 
 <template>
@@ -119,76 +57,7 @@ const modificationsOptions = ref<Array<ModificationsTypeItem>>([
                     }
                 }"
             >
-                <DataTable 
-                    v-if="revenues.length > 0"
-                    v-model:expandedRows="expandedRows"
-                    :value="revenues" 
-                    dataKey="id"
-                    size="large"
-                    removableSort
-                    :loading="loading"
-                >
-                    <Column expander style="width: 5rem" />
-                    <Column field="title" header="Título" sortable></Column>
-
-                    <Column field="amount" header="Valor" sortable>
-                        <template #body="{ data }">
-                            <span>{{ formatCurrency(data.amount) }}</span>
-                        </template>
-                    </Column>
-
-                    <Column field="recurrent" header="Receita fixa" sortable>
-                        <template #body="{ data }">
-                            <span>{{ data.recurrent ? 'Sim' : 'Não' }}</span>
-                        </template>
-                    </Column>
-
-                    <Column field="category" header="Categoria" sortable>
-                        <template #body="{ data }">
-                            <Tag v-if="data.category" :value="data.category?.name" severity="info" />
-                        </template>
-                    </Column>
-
-                    <Column header="Ações">
-                        <template #body="{ data }">
-                            <div class="flex gap-5">
-                                <i 
-                                    class="pi pi-pencil cursor-pointer hover:text-blue-700 transition-all duration-200" 
-                                    @click="onEdit(data)"
-                                ></i>
-                                <i 
-                                    class="pi pi-trash cursor-pointer hover:text-red-700 transition-all duration-200" 
-                                    @click="onDelete(data)"
-                                ></i>
-                            </div>
-                        </template>
-                    </Column>
-
-                    <template #expansion="{ data }">
-                        <Card 
-                            class="!bg-surface-800"
-                            :pt="{
-                                root: { class: 'rounded' },
-                                body: { class: '!p-3' },
-                            }"
-                        >
-                            <template #content>
-                                <span>{{ data.description }}</span>
-                            </template>
-                        </Card>
-                    </template>
-                </DataTable>
-
-                <div v-else class="flex pt-4">
-                    <span class="text-center">Nenhum dado disponível</span>
-                </div>
-
-                <ModificationsTypeDialog
-                    ref="modificationsDialog"
-                    title="Apagar receita?"
-                    description="Essa receita é recorrente, você pode excluir apenas no mês atual, atual e próximas ou todos os registros."
-                    :options="modificationsOptions"
-                />
+                <RevenuesListItems />
             </AccordionContent>
         </AccordionPanel>
     </Accordion>
