f17120ca77a9436856e79be38a62e461302a9421
dukmarques <eduardomarques@outlook.com>
2025-08-24 18:33:31 -0300

    feat(ExpensesStore): implement expenses store with state, getters, and actions

---
+++ src/stores/expenses.ts
@@ -0,0 +1,40 @@
+import type { Expense } from "@/@types/Expenses";
+import { axios } from "@/services/axios";
+import type { AxiosError } from "axios";
+import { defineStore } from "pinia";
+
+export const useExpensesStore = defineStore("expenses", {
+    state: () => ({
+        expenses: [] as Expense[],
+        loading: false,
+        currentSelectedMonth: '' as string | Date,
+    }),
+    getters: {
+        getTotalExpensesAmount: (state) => {
+            return state.expenses.reduce((acc, item) => {
+                const amount = item.amount ? parseFloat(String(item.amount)) : 0;
+                return acc + amount;
+            }, 0);
+        }
+    },
+    actions: {
+        async fetchExpenses(date: string | Date) {
+            this.loading = true;
+            this.currentSelectedMonth = date;
+
+            try {
+                const { data } = await axios.get(`/expenses?date=${this.currentSelectedMonth}&paginate=false`);
+                this.expenses = this.filteredRevenues(data.data);
+            } catch (err: unknown) {
+                const error = err as AxiosError;
+                const errorMessage = (error.response?.data as { message?: string })?.message || "Erro ao buscar as despesas";
+                throw new Error(errorMessage);
+            }
+        },
+
+        filteredRevenues(data: Expense[]) {
+            // TODO: verificar se regra está correta
+            return data.filter(expense => expense.type === 'simple' || !expense.override?.is_deleted);
+        },
+    },
+});
