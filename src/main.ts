16c552e95f3335043c1f87ec86beb89901ff737c
dukmarques <eduardomarques@outlook.com>
2025-08-23 23:56:27 -0300

    refactor: remove vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { VueAxios, axios } from './services/axios';
-
 import App from './App.vue';
 import router from './router/router';
-
 import { configPrimevue } from './plugins/primevue';
 import './styles.css';
 
-//TODO: remove soon
-import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
-import vuetify from './plugins/vuetify';
-//TODO: remove soon
-
 const app = createApp(App);
-
-// TODO: remove soon
-app.use(vuetify);
-app.use(Vue3Toastify, { autoClose: 3000 } as ToastContainerOptions);
-// TODO: remove soon
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
-
 
 configPrimevue(app);
 app.use(pinia);
