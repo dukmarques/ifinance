79113d21aa7d506e50319e0f9baed034e24adc5d
dukmarques <eduardomarques@outlook.com>
2025-09-09 00:37:05 -0300

    refactor(ActionsTypes): remove 'simple' option from ActionDeleteType for consistency

--- src/@types/ActionsTypes.ts
+++ src/@types/ActionsTypes.ts
@@ -1,5 +1,5 @@
 type ActionEditType = 'only_month' | 'current_and_future' | 'all';
 
-type ActionDeleteType = 'simple' | 'only_month' | 'current_and_future' | 'all';
+type ActionDeleteType = 'only_month' | 'current_and_future' | 'all';
 
 export type { ActionEditType, ActionDeleteType };
