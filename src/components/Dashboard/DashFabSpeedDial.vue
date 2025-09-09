10f98b60d65085ad241600a54a2f56c254f4d2e7
dukmarques <eduardomarques@outlook.com>
2025-09-08 22:34:34 -0300

    feat(DashFabSpeedDial): add functionality to create expenses with dialog integration

--- src/components/Dashboard/DashFabSpeedDial.vue
+++ src/components/Dashboard/DashFabSpeedDial.vue
@@ -6,28 +6,13 @@ import { useRevenuesStore } from '@/stores/revenues';
 import { useToast } from '@/composables/useToast';
 import BaseFabSpeedDial from '../BaseForm/BaseFabSpeedDial.vue';
 import ManageRevenuesDialog from '@/components/Revenues/ManageRevenuesDialog.vue';
-
-const createRevenueDialogRef = useTemplateRef<InstanceType<typeof ManageRevenuesDialog>>('createRevenueDialog');
-
-const items = ref<MenuItem[]>([
-    {
-        label: 'Adicionar entrada',
-        icon: 'pi pi-arrow-up',
-        command: () => createRevenueDialogRef.value!.visible = true,
-    },
-    {
-        label: 'Adicionar saída',
-        icon: 'pi pi-arrow-down-right',
-        command: () => console.log('Adicionar saída'),
-    },
-    {
-        label: 'Adicionar saída de cartão',
-        icon: 'pi pi-credit-card',
-        command: () => console.log('Adicionar saída de cartão'),
-    },
-]);
+import ManageExpensesDialog from '../Expenses/ManageExpensesDialog.vue';
+import { useExpensesStore } from '@/stores/expenses';
+import type { Expense } from '@/@types/Expenses';
 
 const { showSuccess, showError } = useToast();
+
+const createRevenueDialogRef = useTemplateRef<InstanceType<typeof ManageRevenuesDialog>>('createRevenueDialog');
 const loadingCreateRevenue = ref(false);
 const revenueStore = useRevenuesStore();
 
@@ -49,6 +34,47 @@ async function handleCreateRevenue(revenue: Revenues) {
         loadingCreateRevenue.value = false;
     }
 }
+
+const createExpenseDialogRef = useTemplateRef<InstanceType<typeof ManageExpensesDialog>>('createExpenseDialog');
+const loadingCreateExpense = ref(false);
+const expenseStore = useExpensesStore();
+
+async function handleCreateExpense(expense: Expense) {
+    try {
+        loadingCreateExpense.value = true;
+        await expenseStore.create(expense);
+        showSuccess({
+            message: 'Sucesso!',
+            description: 'Despesa adicionada com sucesso.'
+        })
+    } catch (err) {
+        const error = err as Error;
+        showError({
+            message: 'Ocorreu um erro!',
+            description: error.message || 'Erro ao adicionar despesa.'
+        })
+    } finally {
+        loadingCreateExpense.value = false;
+    }
+}
+
+const items = ref<MenuItem[]>([
+    {
+        label: 'Adicionar entrada',
+        icon: 'pi pi-arrow-up',
+        command: () => createRevenueDialogRef.value!.visible = true,
+    },
+    {
+        label: 'Adicionar despesa',
+        icon: 'pi pi-arrow-down-right',
+        command: () => createExpenseDialogRef.value!.visible = true,
+    },
+    {
+        label: 'Adicionar saída de cartão',
+        icon: 'pi pi-credit-card',
+        command: () => console.log('Adicionar saída de cartão'),
+    },
+]);
 </script>
 
 <template>
@@ -64,5 +90,11 @@ async function handleCreateRevenue(revenue: Revenues) {
         ref="createRevenueDialog"
         :loading="loadingCreateRevenue"
         :provider="handleCreateRevenue"
+    />
+
+    <ManageExpensesDialog 
+        ref="createExpenseDialog"
+        :loading="loadingCreateExpense"
+        :provider="handleCreateExpense"
     />
 </template>
